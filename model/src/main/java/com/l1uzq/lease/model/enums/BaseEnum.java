6246b5fa569e75487d3b516ca29034be0ca7c604
“lzq” <lzquestc@outlook.com>
2025-03-06 17:32:29 +0800

    更新.gitignore

--- model/src/main/java/com/l1uzq/lease/model/enums/BaseEnum.java
+++
@@ -1,8 +0,0 @@
-package com.l1uzq.lease.model.enums;
-
-public interface BaseEnum {
-
-    Integer getCode();
-
-    String getName();
-}
--- web/web-app/src/main/java/com/l1uzq/lease/web/app/service/SmsService.java
+++
@@ -1,5 +0,0 @@
-package com.l1uzq.lease.web.app.service;
-
-public interface SmsService {
-    void sendCode(String phone, String code);
-}
